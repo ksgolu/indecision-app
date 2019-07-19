4f71757d8cbd4b4ca0669a6db601090ed1ba81f3
ksgolu <satyamshay@gmail.com>
2019-07-20 04:09:00 +0530

    initial commit

---
+++ build/precache-manifest.06042b875f140a9e49f4efe4c771694f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d396c418f10ecdace4e92ef023ddd55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cd68d061d64bc83331b",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "901c09b18818179e842b",
+    "url": "/static/css/main.5474a83c.chunk.css"
+  },
+  {
+    "revision": "5cd68d061d64bc83331b",
+    "url": "/static/js/2.e67d6eba.chunk.js"
+  },
+  {
+    "revision": "901c09b18818179e842b",
+    "url": "/static/js/main.cbf845b8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
